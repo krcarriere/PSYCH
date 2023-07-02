7d800f274b72362bb4ec5dbb11f380bdfecb6be3
Kevin Carriere <krcarriere@users.noreply.github.com>
2023-07-02 09:41:28 -0400

    Delete One_Way_ANOVA directory

--- One_Way_ANOVA/scripts/dplyr-correct.R
+++
@@ -1,6 +0,0 @@
-sum_data <- data %>% group_by(Species) %>% summarize(N = sum(!is.na(Petal.Length)), 
-                                                     Mean = mean(Petal.Length, na.rm=TRUE),
-                                                     SD = sd(Petal.Length, na.rm=TRUE),
-                                                     SE = SD/sqrt(N))
-
-
