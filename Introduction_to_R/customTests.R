ac316117ccf692f6d860ea38aba2d6a479a519bc
Kevin Carriere <krcarriere@users.noreply.github.com>
2023-10-01 20:32:07 -0400

    intro to R
    add script_results_identical, edit yaml

--- Introduction_to_R/customTests.R
+++ Introduction_to_R/customTests.R
@@ -42,3 +42,30 @@ submit_log <- function(){
     browseURL(paste0(pre_fill_link, encoded_log))
   }
 }
+
+
+script_results_identical <- function(result_name) {
+  # Get e
+  e <- get('e', parent.frame())
+  # Get user's result from global
+  if(exists(result_name, globalenv())) {
+    user_res <- get(result_name, globalenv())
+  } else {
+    return(FALSE)
+  }
+  # Source correct result in new env and get result
+  tempenv <- new.env()
+  # Capture output to avoid double printing
+  temp <- capture.output(
+    local(
+      try(
+        source(e$correct_script_temp_path, local = TRUE),
+        silent = TRUE
+      ),
+      envir = tempenv
+    )
+  )
+  correct_res <- get(result_name, tempenv)
+  # Compare results
+  all.equal(user_res, correct_res)
+}
